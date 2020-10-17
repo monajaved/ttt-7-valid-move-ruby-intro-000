389e666095956653a4c67477ebd1ce37c823f588
Mona Javed Syed <monajaved93@gmail.com>
2020-10-17 01:28:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken? && index.between?(1, 9)
+  if index.between?(1, 9)
     return true
   else
     return false
