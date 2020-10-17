a734698c6814f40072bd41a652ae5b3292563025
Mona Javed Syed <monajaved93@gmail.com>
2020-10-17 01:09:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,35 @@
+
+
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if index.between?(1, 9)
+    return true
+    if position_taken?
+      return true
+    else
+      return false
+  end
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
+
+
+
+
+
+
+# def position_taken?(board, index)
+#   if board[index] == " " || board[index] == "" || board[index] == nil
+#     return false
+#   else board[index] == "X" || board[index] == "O"
+#     return true
+#   end
+# end
